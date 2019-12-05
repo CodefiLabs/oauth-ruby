4341230c511a2a95f2714d6bbb916c3039d51041
Stephen Myers <scmyers11@gmail.com>
2019-12-05 13:18:17 -0600

    More debugging

--- lib/oauth/client/net_http.rb
+++ lib/oauth/client/net_http.rb
@@ -108,9 +108,10 @@ private
   end
 
   def set_oauth_query_string
+    puts "ESCAPE?: #{@oauth_helper.escape_token?}"
     oauth_params_str = @oauth_helper.oauth_parameters.map do |k,v|
       puts "KEY: #{k}:#{v}"
-      if k == "oauth_token" && !@oauth_helper.escape_token?
+      if k == 'oauth_token' && !@oauth_helper.escape_token?
         puts "in oauth token #{k}"
         return [escape(k), v] * "=" if k == "oauth_token" && !@oauth_helper.escape_token?
       end
