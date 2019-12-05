e4c17ad77d88f4d9eadba905ea1ea67bf5200851
Stephen Myers <scmyers11@gmail.com>
2019-12-05 14:03:58 -0600

    Change sort order of query string params

--- lib/oauth/client/helper.rb
+++ lib/oauth/client/helper.rb
@@ -31,10 +31,10 @@ module OAuth::Client
         'oauth_body_hash'        => options[:body_hash],
         'oauth_callback'         => options[:oauth_callback],
         'oauth_consumer_key'     => options[:consumer].key,
-        'oauth_token'            => options[:token] ? options[:token].token : '',
+        'oauth_nonce'            => nonce,
         'oauth_signature_method' => options[:signature_method],
         'oauth_timestamp'        => timestamp,
-        'oauth_nonce'            => nonce,
+        'oauth_token'            => options[:token] ? options[:token].token : '',
         'oauth_verifier'         => options[:oauth_verifier],
         'oauth_version'          => (options[:oauth_version] || '1.0'),
         'oauth_session_handle'   => options[:oauth_session_handle]
